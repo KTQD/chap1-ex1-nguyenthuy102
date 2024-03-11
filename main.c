ffa895c4941ced75d1e6c7da23f2d85a36557bb1
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-11 22:34:32 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int main() {
     int height, width;
 
-    // Nhập chiều cao và chiều rộng từ bàn phím
+    // Input height and width from the keyboard
     scanf("%d", &height);
     scanf("%d", &width);
 
-    // Tính chu vi của hình chữ nhật
+    // Calculate the perimeter of the rectangle
     int perimeter = 2 * (height + width);
 
-    // In ra kết quả
+    // Print the result
     printf("%d", perimeter);
 
     return 0;
