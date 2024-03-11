1c0844060bfd77514ccc0eb5874c065a8b5cf137
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-11 22:41:59 +0700

    Update and rename main.c to rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float height, width, perimeter;
+
+    // Input height of the rectangle
+    printf("Input the height of the Rectangle : ");
+    scanf("%f", &height);
+
+    // Input width of the rectangle
+    printf("Input the width of the Rectangle : ");
+    scanf("%f", &width);
+
+    // Calculate perimeter
+    perimeter = 2 * (height + width);
+
+    // Print the perimeter
+    printf("Perimeter of the Rectangle is : %.2f\n", perimeter);
+
+    return 0;
+}
