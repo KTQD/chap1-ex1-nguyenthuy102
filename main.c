20a3496dc8d25c3ebfef6819736e67056faebad6
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-11 22:14:01 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-
 #include <stdio.h>
 
 int main() {
-    printf("Xin chào các bạn1");
+    int height, width;
+
+    // Nhập chiều cao và chiều rộng từ bàn phím
+    printf("Nhập chiều cao của hình chữ nhật: ");
+    scanf("%d", &height);
+    printf("Nhập chiều rộng của hình chữ nhật: ");
+    scanf("%d", &width);
+
+    // Tính chu vi của hình chữ nhật
+    int perimeter = 2 * (height + width);
+
+    // In ra kết quả
+    printf("Chu vi của hình chữ nhật là: %d\n", perimeter);
+
     return 0;
 }
